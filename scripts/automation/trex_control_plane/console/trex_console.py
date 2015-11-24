1e1c11059b7f7fcb5c160dffe2df832a8adf719d
Hanoh Haim <hhaim@cisco.com>
2015-11-24 11:41:19 +0200

    minor console fix

--- scripts/automation/trex_control_plane/console/trex_console.py
+++ scripts/automation/trex_control_plane/console/trex_console.py
@@ -263,12 +263,12 @@ class TRexConsole(TRexGeneralCmd):
         '''stops port(s) transmitting traffic\n'''
         self.stateless_client.cmd_stop_line(line)
 
-    ############# stop
+    ############# pause
     def do_pause(self, line):
         '''pause port(s) transmitting traffic\n'''
         self.stateless_client.cmd_pause_line(line)
 
-    ############# stop
+    ############# resume
     def do_resume(self, line):
         '''resume port(s) transmitting traffic\n'''
         self.stateless_client.cmd_resume_line(line)
