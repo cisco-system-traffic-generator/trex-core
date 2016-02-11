53cb5b1f7340e72374eb30c6b5e495c63776ecd9
Hanoh Haim <hhaim@cisco.com>
2016-02-11 16:30:09 +0200

    remove 1pkt test

--- scripts/automation/regression/unit_tests/functional_tests/stl_basic_tests.py
+++ scripts/automation/regression/unit_tests/functional_tests/stl_basic_tests.py
@@ -172,7 +172,7 @@ class CStlBasic_Test(functional_general_test.CGeneralFunctional_Test):
             ["yaml/imix_1pkt_vm.yaml","-m 1 -l 100",True],
             ["udp_1pkt_pcap.py","-m 1 -l 10",True],
             ["udp_3pkt_pcap.py","-m 1 -l 10",True],
-            ["udp_1pkt_simple.py","-m 1 -l 3",True],
+            #["udp_1pkt_simple.py","-m 1 -l 3",True],
             ["udp_1pkt_pcap_relative_path.py","-m 1 -l 3",True]
 
           ];
