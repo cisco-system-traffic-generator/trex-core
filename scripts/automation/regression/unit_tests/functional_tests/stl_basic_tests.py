1aec60fb7a6bb5dc4143e91e28844dd4fef6ece6
Hanoh Haim <hhaim@cisco.com>
2016-02-11 13:04:30 +0200

    test minor fix

--- scripts/automation/regression/unit_tests/functional_tests/stl_basic_tests.py
+++ scripts/automation/regression/unit_tests/functional_tests/stl_basic_tests.py
@@ -172,11 +172,11 @@ class CStlBasic_Test(functional_general_test.CGeneralFunctional_Test):
 
           ];
 
-        p0=[ ["udp_3pkt_pcap.py","-m 1 -l 10",True] ]
+        p1=[ ["yaml/imix_1pkt_vm.yaml","-m 1 -l 100",True] ]
         
 
         for obj in p:
-            self.run_py_profile_path (obj[0],obj[1],compare =obj[2], do_no_remove=False)
+            self.run_py_profile_path (obj[0],obj[1],compare =obj[2], do_no_remove=True)
 
 
 
