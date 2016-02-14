1f60016f591ebd2e260e501c8c5da10c11d0c7ad
Hanoh Haim <hhaim@cisco.com>
2016-02-14 14:49:51 +0200

    minor fix

--- scripts/automation/trex_control_plane/stl/trex_stl_lib/utils/parsing_opts.py
+++ scripts/automation/trex_control_plane/stl/trex_stl_lib/utils/parsing_opts.py
@@ -69,7 +69,11 @@ match_multiplier_help = """Multiplier should be passed in the following format:
                           [number][<empty> | bps | kbps | mbps | gbps | pps | kpps | mpps | %% ].
                           no suffix will provide an absoulute factor and percentage 
                           will provide a percentage of the line rate. examples
-                          : '-m 10', '-m 10kbps', '-m 10mpps', '-m 23%%' """
+                          '-m 10', '-m 10kbps', '-m 10mpps', '-m 23%%'
+
+                          '-m 23%%'   : is 23% L1 bandwidth 
+                          '-m 23mbps' : is 23mbps in L2 bandwidth (including FCS+4)
+                          """
 
 
 # decodes multiplier
