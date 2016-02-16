101c92840d9818866ea59e866a889a4b01e1fa97
Hanoh Haim <hhaim@cisco.com>
2016-02-16 10:31:33 +0200

    mask hlt test

--- scripts/automation/regression/unit_tests/functional_tests/stl_basic_tests.py
+++ scripts/automation/regression/unit_tests/functional_tests/stl_basic_tests.py
@@ -187,7 +187,7 @@ class CStlBasic_Test(functional_general_test.CGeneralFunctional_Test):
             self.run_py_profile_path (obj[0],obj[1],compare =obj[2], do_no_remove=True)
 
 
-    def test_hlt_profiles (self):
+    def hlt_profiles (self):
         p = (
             ['hlt/hlt_udp_inc_dec_len_9k.py', '-m 1 -l 40', True],
             )
