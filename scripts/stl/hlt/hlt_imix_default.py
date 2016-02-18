ac8d0a812f152de146fc7455e057da31f84c2c83
Yaroslav Brustinov <ybrustin@cisco.com>
2016-02-18 16:59:07 +0200

    imix default profile

---
+++ scripts/stl/hlt/hlt_imix_default.py
@@ -0,0 +1,17 @@
+from trex_stl_lib.trex_stl_hltapi import STLHltStream
+
+
+class STLS1(object):
+
+    def create_streams (self):
+        return STLHltStream(length_mode = 'imix', rate_pps = 2)
+
+    def get_streams (self, direction = 0):
+        return self.create_streams()
+
+# dynamic load - used for trex console or simulator
+def register():
+    return STLS1()
+
+
+
