6d69e95c296d2a2f3081a3934f89f25321e47182
imarom <imarom@cisco.com>
2016-03-03 11:30:54 +0200

    XTERM might not be present on the machine so TUI -x might give exception - fix it

--- scripts/automation/trex_control_plane/stl/console/trex_console.py
+++ scripts/automation/trex_control_plane/stl/console/trex_console.py
@@ -563,11 +563,15 @@ class TRexConsole(TRexGeneralCmd):
             return
 
         if opts.xterm:
+            if not os.path.exists('/usr/bin/xterm'):
+                print format_text("XTERM does not exists on this machine", 'bold')
+                return
 
             info = self.stateless_client.get_connection_info()
 
             exe = './trex-console --top -t -q -s {0} -p {1} --async_port {2}'.format(info['server'], info['sync_port'], info['async_port'])
-            cmd = ['xterm', '-geometry', '111x47', '-sl', '0', '-title', 'trex_tui', '-e', exe]
+            cmd = ['/usr/bin/xterm', '-geometry', '111x47', '-sl', '0', '-title', 'trex_tui', '-e', exe]
+
             self.terminal = subprocess.Popen(cmd)
 
             return
