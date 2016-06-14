73fdde4e6b2d67ae3587a7133975e41deb46e2fd
Hanoh Haim <hhaim@cisco.com>
2016-06-14 13:06:21 +0300

    fix typo

--- src/main_dpdk.cpp
+++ src/main_dpdk.cpp
@@ -4782,7 +4782,7 @@ int main_test(int argc , char * argv[]){
 
     /* disable WD if needed */
     //CGlobalInfo::m_options.preview.getWDDisable()?false:true
-    g_trex.m_watchdog.init(false); /* always disable - due to trex-211 
+    g_trex.m_watchdog.init(false); /* always disable - due to trex-211 */
 
     /* this will give us all cores - master + tx + latency */
     g_trex.m_watchdog.mark_pending_monitor(g_trex.m_max_cores);
