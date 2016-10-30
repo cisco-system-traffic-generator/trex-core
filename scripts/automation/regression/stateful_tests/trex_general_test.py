d821c0b9e8afff312f79faf4d9db9a1401b8a216
imarom <imarom@cisco.com>
2016-10-30 18:28:40 +0200

    for now shutdown stateful GA
    Signed-off-by: imarom <imarom@cisco.com>

--- scripts/automation/regression/stateful_tests/trex_general_test.py
+++ scripts/automation/regression/stateful_tests/trex_general_test.py
@@ -175,10 +175,11 @@ class CTRexGeneral_Test(unittest.TestCase):
         # report benchmarks
         if self.GAManager:
             try:
-                setup_test = '%s.%s' % (CTRexScenario.setup_name, self.get_name())
-                self.GAManager.gaAddAction(Event = 'stateful_test', action = setup_test, label = 'bw_per_core', value = int(test_norm_cpu))
-                self.GAManager.gaAddAction(Event = 'stateful_test', action = setup_test, label = 'bw_per_core_exp', value = int(expected_norm_cpu))
-                self.GAManager.emptyAndReportQ()
+                pass
+                #setup_test = '%s.%s' % (CTRexScenario.setup_name, self.get_name())
+                #self.GAManager.gaAddAction(Event = 'stateful_test', action = setup_test, label = 'bw_per_core', value = int(test_norm_cpu))
+                #self.GAManager.gaAddAction(Event = 'stateful_test', action = setup_test, label = 'bw_per_core_exp', value = int(expected_norm_cpu))
+                #self.GAManager.emptyAndReportQ()
             except Exception as e:
                 print('Sending GA failed: %s' % e)
 
