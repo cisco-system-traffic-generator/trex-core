cfcbe7a0d5085af3d5fe1c9127b9237fa66408b2
Yaroslav Brustinov <ybrustin@cisco.com>
2017-02-02 11:57:17 +0200

    fixed missing .so files due to ignored by .gitignore + fixed path
    Change-Id: If014548f3d007f9eda6d54332ac04952ea91f751
    Signed-off-by: Yaroslav Brustinov <ybrustin@cisco.com>

--- scripts/automation/trex_control_plane/server/outer_packages.py
+++ scripts/automation/trex_control_plane/server/outer_packages.py
@@ -3,11 +3,12 @@
 import sys
 import os
 python_ver = 'python%s' % sys.version_info.major
+ucs_ver = 'ucs2' if sys.maxunicode == 65535 else 'ucs4'
 
 CURRENT_PATH         = os.path.dirname(os.path.realpath(__file__))                
 ROOT_PATH            = os.path.abspath(os.path.join(CURRENT_PATH, os.pardir))     # path to trex_control_plane directory
 PATH_TO_PYTHON_LIB   = os.path.abspath(os.path.join(ROOT_PATH, os.pardir, os.pardir, 'external_libs'))
-PATH_TO_PLATFORM_LIB = os.path.abspath(os.path.join(PATH_TO_PYTHON_LIB, 'pyzmq-14.5.0', python_ver , 'fedora18', '64bit'))
+ZMQ_PATH = os.path.abspath(os.path.join(PATH_TO_PYTHON_LIB, 'pyzmq-14.5.0', python_ver, ucs_ver, '64bit'))
 
 SERVER_MODULES = ['enum34-1.0.4',
                   'zmq',
@@ -19,10 +20,13 @@ SERVER_MODULES = ['enum34-1.0.4',
 
 
 def import_server_modules():
-    # must be in a higher priority 
-    sys.path.insert(0, PATH_TO_PYTHON_LIB)
-    sys.path.insert(0, PATH_TO_PLATFORM_LIB)
-    sys.path.append(ROOT_PATH)
+    # must be in a higher priority
+    if PATH_TO_PYTHON_LIB not in sys.path:
+        sys.path.insert(0, PATH_TO_PYTHON_LIB)
+    if ZMQ_PATH not in sys.path:
+        sys.path.insert(0, ZMQ_PATH)
+    if ROOT_PATH not in sys.path:
+        sys.path.append(ROOT_PATH)
     import_module_list(SERVER_MODULES)
 
 
@@ -31,7 +35,8 @@ def import_module_list(modules_list):
     for p in modules_list:
         full_path = os.path.join(PATH_TO_PYTHON_LIB, p)
         fix_path = os.path.normcase(full_path)
-        sys.path.insert(1, full_path)
+        if full_path not in sys.path:
+            sys.path.insert(1, full_path)
 
 
 import_server_modules()
