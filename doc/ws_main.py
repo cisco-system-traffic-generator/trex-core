5bd1b14228f73af4c50412ebbc02cf5d6515c0a3
Ido Barnea <ibarnea@cisco.com>
2017-02-07 14:41:58 +0200

    VF interfaces documentation and benchmark
    Signed-off-by: Ido Barnea <ibarnea@cisco.com>

--- doc/ws_main.py
+++ doc/ws_main.py
@@ -975,6 +975,9 @@ def build(bld):
         source='trex_vm_manual.asciidoc waf.css', target='trex_vm_manual.html',scan=ascii_doc_scan)
 
     bld(rule=convert_to_html_toc_book,
+        source='trex_vm_bench.asciidoc waf.css', target='trex_vm_bench.html',scan=ascii_doc_scan)
+
+    bld(rule=convert_to_html_toc_book,
         source='trex_stateless.asciidoc waf.css', target='trex_stateless.html',scan=ascii_doc_scan);
 
     bld(rule=convert_to_html_toc_book,
