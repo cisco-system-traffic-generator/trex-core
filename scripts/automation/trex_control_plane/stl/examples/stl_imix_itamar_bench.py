1b209b8ecce711c3af882ab55a9f9ac0cfc19a9a
itraviv <itraviv@cisco.com>
2017-04-27 18:18:12 +0300

    finished logic of ndr algorithm
    Signed-off-by: itraviv <itraviv@cisco.com>

--- scripts/automation/trex_control_plane/stl/examples/stl_imix_itamar_bench.py
+++ scripts/automation/trex_control_plane/stl/examples/stl_imix_itamar_bench.py
@@ -34,17 +34,19 @@ class Rate:
 
 class NdrBench:
     def __init__(self, stl_client, ports, iteration_duration, iteration_precision, q_ful_resolution,
-                 first_run_duration, pdr, verbose=True):
+                 first_run_duration, pdr, pdr_error=1, max_iterations=10, verbose=True):
         self.verbose = verbose
         self.stl_client = stl_client
         self.ports = ports
         self.max_rate = 0  # in mb
         self.iteration_duration = iteration_duration
-        self.q_ful_iteration_precision = iteration_precision
+        self.iteration_precision = iteration_precision
         self.q_ful_resolution = q_ful_resolution
         self.first_run_duration = first_run_duration
         self.no_q_ful_point_percent = 0  # percent of max rate
         self.pdr = pdr  # desired percent of drop-rate. pdr = 0 is NO drop-rate
+        self.pdr_error = pdr_error
+        self.max_iterations = max_iterations
 
     def __find_max_rate(self):
         c = self.stl_client
@@ -54,123 +56,104 @@ class NdrBench:
         c.start(ports=ports, mult="100%", duration=duration, total=True)
         time.sleep(float(duration) / 2.00)
         stats = c.get_stats()
+        c.clear_stats()
+        c.stop(ports=ports)
         max_rate = stats['total']['tx_bps_L1']
         q_ful_packets = stats['global']['queue_full']
         opackets = stats[ports[0]]['opackets']
+        ipackets = stats[1]['ipackets']
+        lost_p = opackets - ipackets
+        lost_p_percentage = (float(lost_p) / float(opackets)) * 100.00
         q_ful_percentage = float((q_ful_packets / float(opackets)) * 100.000)
-        if q_ful_percentage <= self.q_ful_resolution:
-            self.no_q_ful_point_percent = 100.00
-            if self.verbose:
-                print 'queue full percentage is: %0.4f' % q_ful_percentage
         max_rate_mb = float(max_rate) / 1000000.00
         self.max_rate = max_rate_mb
         if self.verbose:
             print "max rate in Mb: %0.4f \n" % max_rate_mb
-        c.clear_stats()
-        c.stop(ports=ports)
+            print "drop rate is: %0.4f %% of opackets" % lost_p_percentage
+            print "q ful is %0.4f %% of opackets" % q_ful_percentage
+        if lost_p_percentage < 0:
+            lost_p_percentage = 0
+        return q_ful_percentage, lost_p_percentage
 
-    # should find max_rate first
-    def q_ful_perf_iterations(self, rate_interval, iteration_precision):
+    def perfRun(self, rate_mb):
+        self.stl_client.clear_stats()
+        self.stl_client.start(ports=self.ports, mult=(str(rate_mb) + "mbps"), duration=self.iteration_duration,
+                              total=True)
+        time.sleep(self.iteration_duration / 2)
+        stats = self.stl_client.get_stats()
+        self.stl_client.stop(ports=self.ports)
+        opackets = stats[self.ports[0]]['opackets']
+        ipackets = stats[1]['ipackets']
+
+        lost_p = opackets - ipackets
+        lost_p_percentage = (float(lost_p) / float(opackets)) * 100.00
+        if lost_p_percentage < 0:
+            lost_p_percentage = 0
+        q_ful_packets = stats['global']['queue_full']
+        q_ful_percentage = float((q_ful_packets / float(opackets)) * 100.000)
+
+        return q_ful_percentage, lost_p_percentage
+
+    def perf_run_interval(self, high_bound, low_bound):
+        """
+
+        Args:
+            high_bound: in percents of rate
+            low_bound: in percents of rate
+
+        Returns: ndr in interval between low and high bounds (opt), q_ful(%) at opt and drop_rate(%) at opt
+
+        """
         max_rate = Rate(self.max_rate)
-        high_bound = max_rate.convert_percent_to_rate(rate_interval[0])
-        low_bound = max_rate.convert_percent_to_rate(rate_interval[1])
-        rate_delta = max_rate.convert_percent_to_rate(iteration_precision)
-        q_ful_percentage = 100
-        iterations = 0
-        print "max rate is: %0.4f " % float(max_rate.rate)
-        print "starting rate is: %0.4f " % (high_bound - rate_delta)
-        rate = high_bound
-        rate_percentage_opt = rate_interval[1]  # low_bound
-        while float(q_ful_percentage) > float(self.q_ful_resolution):
-            iterations += 1
-            rate -= rate_delta
-            print("rate %0.4f low bound %0.4f" % (rate, low_bound))
-            if float(rate) <= float(low_bound):
-                break
-            self.stl_client.clear_stats()
-            rate_percentage = max_rate.convert_rate_to_percent_of_max_rate(rate)
-            test_duration = (100 / float(rate_percentage)) * self.iteration_duration
-            print "***** iteration %d *****" % iterations
-            print("starting traffic at %0.4f mbps which is %0.4f %% of max rate for %0.1f seconds" % (
-                rate, rate_percentage, test_duration))
-            self.stl_client.start(ports=self.ports, mult=(str(rate) + "mbps"), duration=test_duration, total=True)
-            self.stl_client.wait_on_traffic(ports=self.ports)
-            stats = self.stl_client.get_stats()
-            q_ful_packets = stats['global']['queue_full']
-            opackets = stats[self.ports[0]]['opackets']
-            print('queue full is: %d' % q_ful_packets)
-            q_ful_percentage = float((q_ful_packets / float(opackets)) * 100.000)
-            print (
-                'queue full percentage is: %0.4f, resolution is: %0.4f \n' % (q_ful_percentage, self.q_ful_resolution))
-            print("rate_percentage %0.4f  rate_percentage_opt %0.4f\n\n" % (rate_percentage, rate_percentage_opt))
-            if float(q_ful_percentage) <= float(self.q_ful_resolution):
-                if rate_percentage > rate_percentage_opt:
-                    rate_percentage_opt = rate_percentage
-            time.sleep(2)
-        return rate_percentage_opt
+        # rate_delta = max_rate.convert_percent_to_rate(self.iteration_precision)
+        opt = 0  # percent
+        q_ful_opt = 0
+        drop_rate_opt = 0
+        iteration = 0
+        while iteration <= self.max_iterations:
+            running_rate_percent = float((high_bound + low_bound)) / 2.00
+            running_rate = max_rate.convert_percent_to_rate(running_rate_percent)
+            q_ful_percentage, lost_p_percentage = self.perfRun(running_rate)
+            if self.verbose:
+                print "*** iteration %d***\n" % iteration
+                print("running at %0.4f %% of line rate which is: %0.4f\n" % (running_rate_percent, running_rate))
+                print("drop rate is: %0.4f %% and q ful is %0.4f %%\n" % (lost_p_percentage, q_ful_percentage))
+            if q_ful_percentage <= self.q_ful_resolution and lost_p_percentage <= self.pdr:
+                if running_rate_percent > opt:
+                    if abs(running_rate_percent - opt) <= self.pdr_error:
+                        break
+                    opt = running_rate_percent
+                    q_ful_opt = q_ful_percentage
+                    drop_rate_opt = lost_p_percentage
+                    low_bound = running_rate_percent
+                    iteration += 1
+                    continue
+                else:
+                    break
+            high_bound = running_rate_percent
+            iteration += 1
+        return opt, q_ful_opt, drop_rate_opt
 
     def find_ndr(self):
-        # duration should be at least 10 seconds
-        lost_p_percentage = 100
-        print "no q ful point percent %0.4f : " % self.no_q_ful_point_percent
-        no_q_ful_rate_temp = Rate(self.max_rate).convert_percent_to_rate(self.no_q_ful_point_percent)
-        no_q_ful_rate = Rate(no_q_ful_rate_temp)
-        running_rate = no_q_ful_rate.rate
-        while self.pdr < lost_p_percentage:
-            rate_percentage = no_q_ful_rate.convert_rate_to_percent_of_max_rate(running_rate)
-            print("starting traffic at %0.4f mbps which is %0.4f %% of max rate for %0.1f seconds" % (
-                running_rate, rate_percentage, self.iteration_duration))
-            self.stl_client.start(ports=self.ports, mult=(str(running_rate) + "mbps"), duration=self.iteration_duration,
-                                  total=True)
-            self.stl_client.clear_stats()
-            time.sleep(5)
-            stats = self.stl_client.get_stats()
-            self.stl_client.stop(ports=self.ports)
-            # print "******* NDR printing"
-            # print ports
-            # pprint(stats)
-            opackets = stats[self.ports[0]]['opackets']
-            ipackets = stats[1]['ipackets']
+        q_ful_percent, drop_percent = self.__find_max_rate()
+        max_rate = Rate(self.max_rate)
+        if drop_percent > self.pdr:
+            assumed_rate_percent = 100 - drop_percent  # percent calculation
+            if self.verbose:
+                assumed_rate_mb = max_rate.convert_percent_to_rate(assumed_rate_percent)
+                print "assumed rate percent: %0.4f " % assumed_rate_percent
+                print "assumed rate mb: %0.4f " % assumed_rate_mb
+            opt, q_ful_opt, drop_rate_opt = self.perf_run_interval(assumed_rate_percent,
+                                                                   assumed_rate_percent - self.pdr_error)
+        if q_ful_percent >= self.q_ful_resolution:
+            opt, q_ful_opt, drop_rate_opt = self.perf_run_interval(100.00, 0.00)
 
-            lost_p = opackets - ipackets
-
-            lost_p_percentage = (float(lost_p) / float(opackets)) * 100.00
-            print(
-                "opackets is: %0.4f, ipackets is: %0.4f, lost_percenage is: %0.4f\n" % (
-                    opackets, ipackets, lost_p_percentage))
-            if round(self.pdr, 4) >= round(float(lost_p_percentage), 4):
-                print ("pdr found \n pdr: %0.4f, drop percentage: %0.4f \n" % (self.pdr, lost_p_percentage))
-                break
-            else:
-                print("pdr not found yet \npdr: %0.4f, drop percentage: %0.4f \n" % (self.pdr, lost_p_percentage))
-            rate_delta = no_q_ful_rate.convert_percent_to_rate(lost_p_percentage)
-            running_rate -= rate_delta
-        return  running_rate
-
-    def find_no_q_full_rate(self):
-        self.__find_max_rate()
-        print "this is max rate i found %0.4f " % self.max_rate
-        if not self.no_q_ful_point_percent:
-            if self.q_ful_iteration_precision <= 10:
-                self.no_q_ful_point_percent = self.q_ful_perf_iterations([100.00, 0.0], 10)
-            if self.q_ful_iteration_precision <= 5:
-                q_ful_low_bound = self.no_q_ful_point_percent
-                q_ful_high_bound = q_ful_low_bound + 10
-                self.no_q_ful_point_percent = self.q_ful_perf_iterations([q_ful_high_bound, q_ful_low_bound], 5)
-                q_ful_low_bound = self.no_q_ful_point_percent
-                q_ful_high_bound = self.no_q_ful_point_percent + 5
-                self.no_q_ful_point_percent = self.q_ful_perf_iterations([q_ful_high_bound, q_ful_low_bound],
-                                                                         self.q_ful_iteration_precision)
-
-
-
-
-
-
-
-
-
-
+        print("drop rate is: %0.4f %%" % drop_rate_opt)
+        print (
+            'queue full percentage is: %0.4f, resolution is: %0.4f \n' % (q_ful_opt, self.q_ful_resolution))
+        print("max rate %0.4f \n" % self.max_rate)
+        print("rate_percentage %0.4f \n" % opt)
+        return drop_rate_opt, opt
 
 
 # IMIX test
@@ -212,11 +195,10 @@ def imix_test(server, mult):
         c.add_streams(streams, ports=dir_0)
         c.add_streams(streams, ports=dir_1)
 
-        b = NdrBench(c,dir_0,iteration_duration=20,iteration_precision=1,q_ful_resolution=0.5,first_run_duration=20,pdr=0.1,verbose=True)
-        b.find_no_q_full_rate()
-        ndr_rate = b.find_ndr()
-
-        print("PDR of %0.4f is at rate: %0.4f" % (b.pdr,ndr_rate))
+        b = NdrBench(c, dir_0, iteration_duration=20, iteration_precision=1, q_ful_resolution=2,
+                     first_run_duration=20, pdr=0.1, verbose=True)
+        pdr, ndr_rate = b.find_ndr()
+        print("PDR of %0.4f is at rate: %0.4f" % (pdr, ndr_rate))
     except STLError as e:
         passed = False
         print(e)
