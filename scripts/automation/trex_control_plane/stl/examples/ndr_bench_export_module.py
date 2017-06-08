f8c49c2edaba2142a85aa95754e19e5bec1c7fa0
itraviv <itraviv@cisco.com>
2017-06-08 18:23:14 +0300

    small additions to the export tool, added generation of report for bench results
    Signed-off-by: itraviv <itraviv@cisco.com>

--- scripts/automation/trex_control_plane/stl/examples/ndr_bench_export_module.py
+++ scripts/automation/trex_control_plane/stl/examples/ndr_bench_export_module.py
@@ -2,8 +2,9 @@ import stl_ndr_bench_tool as ndrtool
 from pprint import pprint
 
 import pandas as pd
+
 config = {"server": 'csi-trex-08', "vm": "cached", "latency": True, "drop_rate_interval": 10,
-          "pkt_size": "128", "max_iterations": 10, "iteration_duration": 20.0, "q_ful_resolution": 2.0,
+          "pkt_size": "128", "max_iterations": 1, "iteration_duration": 20.0, "q_ful_resolution": 2.0,
           "first_run_duration": 20.0, "ports_list": [0, 3], "pdr": 0.1, "pdr_error": 1.0,
           "ndr_results": 1, "verbose": True}
 
@@ -14,7 +15,6 @@ print("this is results:\n")
 pprint(results)
 
 s = pd.Series(results['results'])
-results_dframe = pd.DataFrame(s)
-results_dframe.to_csv("myfile.csv")
-
+results_dframe = pd.DataFrame({results['results']['title']: s})
 print results_dframe
+results_dframe.to_csv("myfile.csv")
