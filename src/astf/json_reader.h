3026aa212e8952bd241a2797c5075783f198a8dc
Hanoh Haim <hhaim@cisco.com>
2017-08-23 16:17:59 +0300

    tcp - minor add license
    Signed-off-by: Hanoh Haim <hhaim@cisco.com>

--- src/astf/json_reader.h
+++ src/astf/json_reader.h
@@ -1,3 +1,27 @@
+#ifndef ASTF_JSON_READER_H 
+#define ASTF_JSON_READER_H
+
+/*
+ Ido Barnea
+ Cisco Systems, Inc.
+*/
+
+/*
+Copyright (c) 2015-2017 Cisco Systems, Inc.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
 #include <json/json.h>
 #include <iostream>
 #include <string>
@@ -146,3 +170,5 @@ class CJsonData {
     std::mutex m_mtx[2];
     CTcpData m_tcp_data[2];
 };
+
+#endif
