5560ab4fe6318a678de697532b68f5591ae219be
Ido Barnea <ibarnea@cisco.com>
2017-09-27 17:18:29 +0300

    TCP python API: issue error for corrupted cap files
    Signed-off-by: Ido Barnea <ibarnea@cisco.com>

--- scripts/automation/trex_control_plane/astf/trex_astf_lib/cap_handling.py
+++ scripts/automation/trex_control_plane/astf/trex_astf_lib/cap_handling.py
@@ -1,6 +1,6 @@
-import sys
 from scapy.all import RawPcapReader, Ether
 from .trex_astf_exceptions import ASTFError
+
 
 class CPacketData():
     def __init__(self, direction, payload):
@@ -84,12 +84,14 @@ class _CPcapReader_help(object):
             self._pkts[i].dump()
 
     def analyze(self):
+        pkt_num = 0
         pcap = RawPcapReader(self.file_name).read_all()
         if not pcap:
             self.fail('Empty pcap {0}'.format(self.file_name))
 
         l4_type = None
         for index, (raw_pkt, _) in enumerate(pcap):
+            pkt_num += 1
             scapy_pkt = Ether(raw_pkt)
 
             # l3
@@ -141,9 +143,11 @@ class _CPcapReader_help(object):
                             if self.state != _CPcapReader_help.states["syn"]:
                                 self.fail('Packet #%s is SYN+ACK, but there was already SYN+ACK in cap file' % index)
                         self.state = _CPcapReader_help.states["syn+ack"]
+                        exp_s_seq = tcp.seq + 1
                     # SYN - no ACK. Should be first packet client->server
                     else:
                         self.c_tcp_win = tcp.window
+                        exp_c_seq = tcp.seq + 1
                         # allowing syn retransmission because cap2/https.pcap contains this
                         if self.state > _CPcapReader_help.states["syn"]:
                             self.fail('Packet #%s is TCP SYN, but there was already TCP SYN in cap file' % index)
@@ -178,31 +182,30 @@ class _CPcapReader_help(object):
             self.total_payload_len += l4_payload_len
             self._pkts.append(CPacketData(direction, bytes(l4.payload)[0:l4_payload_len]))
 
+            # special handling for TCP FIN
+            if l4.flags & 0x01:
+                l4_payload_len = 1
+            # verify there is no packet loss or retransmission in cap file
+            # don't check for SYN
+            if tcp and (l4.flags & 0x02) == 0:
+                if l4.sport == self.s_port:
+                    if exp_c_seq != tcp.seq:
+                        self.fail("""TCP seq in packet {0} is {1}. We expected {2}. Please check that there are no packet
+                        loss or retransmission in cap file"""
+                                  .format(pkt_num, tcp.seq, exp_c_seq))
+                    exp_c_seq = tcp.seq + l4_payload_len
+                else:
+                    if exp_s_seq != tcp.seq:
+                        self.fail("""TCP seq in packet {0} is {1}. We expected {2}. Please check that there are
+                        no packet loss or retransmission in cap file"""
+                                  .format(pkt_num, tcp.seq, exp_s_seq))
+                    exp_s_seq = tcp.seq + l4_payload_len
+
     def gen_prog_file_header(self, out):
         out.write("import astf_path\n")
         out.write("from trex_astf_lib.api import *\n")
         out.write("import json\n")
         out.write("\n")
-
-#     ??? think if we need this
-#    def gen_prog_file(self, out, origin="c"):
-#        self.gen_prog_file_header(out)
-#        prog_var_name = "prog"
-#        c_tcp_var_name = "c_tcp"
-#        s_tcp_var_name = "s_tcp"
-#
-#        c_tcp_info = ASTFTCPInfo(window=self.c_tcp_win)
-#        c_tcp_info.dump(out, c_tcp_var_name)
-#        s_tcp_info = ASTFTCPInfo(window=self.s_tcp_win)
-#        s_tcp_info.dump(out, s_tcp_var_name)
-#
-#        prog = ASTFProgram()
-#        prog.create_cmds(self._pkts, origin)
-#        print(prog.to_json())
-#        prog.dump(out, prog_var_name)
-#
-#        out.write("prof = ASTFProfile(program={0}, tcp_info={1})\n"
-#                  .format(prog_var_name, c_tcp_var_name))
 
 
 class CPcapReader(object):
