cisco-system-traffic-generator/trex-core

commit f7a0860ac6e732872d594d310ebace8ed691aa14

Receive all ether types when doing set_rcv_all on X710
Signed-off-by: Ido Barnea <ibarnea@cisco.com>

src/main_dpdk.cpp
@@ -7817,7 +7817,16 @@ int CTRexExtendedDriverBase40G::set_rcv_all(CPhyEthIF * _if, bool set_on) {
 
     enum rte_filter_op op = set_on ? RTE_ETH_FILTER_ADD : RTE_ETH_FILTER_DELETE;
 
-    add_del_eth_type_rule(repid, op, EthernetHeader::Protocol::ARP);
+    uint16_t all_eth_types[]  = {
+        0x0800, 0x0806, 0x0842, 0x22F3, 0x22EA, 0x6003, 0x8035, 0x809B, 0x80F3, 0x8100,
+        0x8137, 0x8204, 0x86DD, 0x8808, 0x8809, 0x8819, 0x8847, 0x8848, 0x8863, 0x8864,
+        0x886D, 0x8870, 0x887B, 0x888E, 0x8892, 0x889A, 0x88A2, 0x88A4, 0x88A8, 0x88AB,
+        0x88B8, 0x88B9, 0x88BA, 0x88CC, 0x88CD, 0x88DC, 0x88E1, 0x88E3, 0x88E5, 0x88E7,
+        0x88F7, 0x88FB, 0x8902, 0x8906, 0x8914, 0x8915, 0x891D, 0x892F, 0x9000, 0x9100,
+    };
+    for (int i = 0; i < sizeof(all_eth_types)/sizeof(uint16_t); i++) {
+        add_del_eth_type_rule(repid, op, all_eth_types[i]);
+    }
 
     if (set_on) {
         i40e_trex_fdir_reg_init(repid, I40E_TREX_INIT_RCV_ALL);

commit e32a88eb1ab48c8918eb6ad2837a0c095f2cd33e

Moving code around to overcome strange performance issue introduced by f7a0860ac6e732872d594d310ebace8ed691aa14
Signed-off-by: Ido Barnea <ibarnea@cisco.com>

src/main_dpdk.cpp
@@ -143,6 +143,14 @@ static char g_image_postfix[10];
 static CPciPorts port_map;
 #define TREX_NAME "_t-rex-64"
 
+static uint16_t all_eth_types[]  = {
+    0x0800, 0x0806, 0x0842, 0x22F3, 0x22EA, 0x6003, 0x8035, 0x809B, 0x80F3, 0x8100,
+    0x8137, 0x8204, 0x86DD, 0x8808, 0x8809, 0x8819, 0x8847, 0x8848, 0x8863, 0x8864,
+    0x886D, 0x8870, 0x887B, 0x888E, 0x8892, 0x889A, 0x88A2, 0x88A4, 0x88A8, 0x88AB,
+    0x88B8, 0x88B9, 0x88BA, 0x88CC, 0x88CD, 0x88DC, 0x88E1, 0x88E3, 0x88E5, 0x88E7,
+    0x88F7, 0x88FB, 0x8902, 0x8906, 0x8914, 0x8915, 0x891D, 0x892F, 0x9000, 0x9100,
+};
+
 class CTRexExtendedDriverBase {
 protected:
     enum {
@@ -7817,13 +7825,6 @@ int CTRexExtendedDriverBase40G::set_rcv_all(CPhyEthIF * _if, bool set_on) {
 
     enum rte_filter_op op = set_on ? RTE_ETH_FILTER_ADD : RTE_ETH_FILTER_DELETE;
 
-    uint16_t all_eth_types[]  = {
-        0x0800, 0x0806, 0x0842, 0x22F3, 0x22EA, 0x6003, 0x8035, 0x809B, 0x80F3, 0x8100,
-        0x8137, 0x8204, 0x86DD, 0x8808, 0x8809, 0x8819, 0x8847, 0x8848, 0x8863, 0x8864,
-        0x886D, 0x8870, 0x887B, 0x888E, 0x8892, 0x889A, 0x88A2, 0x88A4, 0x88A8, 0x88AB,
-        0x88B8, 0x88B9, 0x88BA, 0x88CC, 0x88CD, 0x88DC, 0x88E1, 0x88E3, 0x88E5, 0x88E7,
-        0x88F7, 0x88FB, 0x8902, 0x8906, 0x8914, 0x8915, 0x891D, 0x892F, 0x9000, 0x9100,
-    };
     for (int i = 0; i < sizeof(all_eth_types)/sizeof(uint16_t); i++) {
         add_del_eth_type_rule(repid, op, all_eth_types[i]);
     }