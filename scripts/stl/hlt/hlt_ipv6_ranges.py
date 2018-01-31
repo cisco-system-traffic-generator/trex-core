fb4dace311a403b3dc90bb5f782f1bf62bbf360a
Yaroslav Brustinov <ybrustin@cisco.com>
2018-01-31 11:53:34 +0200

    HLTAPI fix - flow stats not applied to IPv6 Added flow stats to one of profiles with IPv6.
    Change-Id: I3fbe8a126af4df73ba193b9209b3b7992728bfa8

--- scripts/stl/hlt/hlt_ipv6_ranges.py
+++ scripts/stl/hlt/hlt_ipv6_ranges.py
@@ -4,10 +4,12 @@ from trex_stl_lib.trex_stl_hltapi import STLHltStream
 class STLS1(object):
     '''
     Eth/IPv6/UDP stream with VM, to change the ipv6 addr (only 32 lsb)
+    Has per-stream stats.
     '''
 
     def get_streams (self, direction = 0, **kwargs):
         return STLHltStream(l3_protocol = 'ipv6', l3_length = 150, l4_protocol = 'udp',
+                            flow_stats_id = 23,
                             ipv6_src_addr = '1111:2222:3333:4444:5555:6666:7777:8888',
                             ipv6_dst_addr = '1111:1111:1111:1111:1111:1111:1111:1111',
                             ipv6_src_mode = 'increment', ipv6_src_step = 5, ipv6_src_count = 10,
