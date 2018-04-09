ab1d3372374dd7d1c6553ba623eb1a79050ecc20
Yaroslav Brustinov <ybrustin@cisco.com>
2018-04-09 18:29:07 +0300

    At start of TRex server, do not search for Python in network path

--- scripts/find_python.sh
+++ scripts/find_python.sh
@@ -1,6 +1,10 @@
-#!/bin/bash
+#!/bin/bash +e
 
-set +e
+PYTHONS2_LOCAL=( python /usr/bin/python )
+PYTHONS2=( ${PYTHONS2_LOCAL[*]} /router/bin/python-2.7.4 )
+
+PYTHONS3_LOCAL=( python3 /usr/bin/python3 )
+PYTHONS3=( ${PYTHONS3_LOCAL[*]} /auto/proj-pcube-b/apps/PL-b/tools/python3.4/bin/python3 )
 
 # function finds python2
 function find_python2 {
@@ -10,15 +14,15 @@ function find_python2 {
     fi
 
     # try different Python paths
-    PYTHONS=( python /usr/bin/python /router/bin/python-2.7.4 )
+    PYTHONS="$@"
     for PYTHON in ${PYTHONS[*]}; do
-        $PYTHON -c "import sys; ver = sys.version_info[0] * 10 + sys.version_info[1];sys.exit(ver < 27)" > /dev/null 2>&1
+        $PYTHON -c "import sys; ver = sys.version_info[0] * 10 + sys.version_info[1];sys.exit(ver < 27)" &> /dev/null
         if [ $? -eq 0 ]; then
             return
         fi
     done;
 
-    echo "*** Python version is too old, 2.7 at least is required"
+    echo "*** Python2 version is too old, 2.7 at least is required"
     exit -1
 }
 
@@ -31,9 +35,9 @@ function find_python3 {
     fi
 
     # try different Python3 paths
-    PYTHONS=( python3 /usr/bin/python3 /auto/proj-pcube-b/apps/PL-b/tools/python3.4/bin/python3 )
+    PYTHONS="$@"
     for PYTHON in ${PYTHONS[*]}; do
-        $PYTHON -c "import sys; ver = sys.version_info[0] * 10 + sys.version_info[1];sys.exit(ver != 34 and ver != 35)" > /dev/null 2>&1
+        $PYTHON -c "import sys; ver = sys.version_info[0] * 10 + sys.version_info[1];sys.exit(ver != 34 and ver != 35)" &> /dev/null
         if [ $? -eq 0 ]; then
             return
         fi
@@ -45,10 +49,26 @@ function find_python3 {
 
 case "$1" in
     "--python2") # we want python2
-        find_python2
+        find_python2 ${PYTHONS2[*]}
         ;;
     "--python3") # we want python3
-        find_python3
+        find_python3 ${PYTHONS3[*]}
+        ;;
+    "--local") # any local, 50/50 try Python2 first
+        case $(($RANDOM % 10)) in
+            [5-9])
+                (find_python2 ${PYTHONS2_LOCAL[*]}) &> /dev/null && find_python2 ${PYTHONS2_LOCAL[*]} && return
+                (find_python3 ${PYTHONS3_LOCAL[*]}) &> /dev/null && find_python3 ${PYTHONS3_LOCAL[*]} && return
+                echo "Python versions 2.7 or 3.4 or 3.5 required"
+                exit -1
+                ;;
+            *)
+                (find_python3 ${PYTHONS3_LOCAL[*]}) &> /dev/null && find_python3 ${PYTHONS3_LOCAL[*]} && return
+                (find_python2 ${PYTHONS2_LOCAL[*]}) &> /dev/null && find_python2 ${PYTHONS2_LOCAL[*]} && return
+                echo "Python versions 2.7 or 3.4 or 3.5 required"
+                exit -1
+                ;;
+        esac
         ;;
     *)
         if [ -z "$PYTHON" ]; then # no python env. var
@@ -56,16 +76,16 @@ case "$1" in
                 imarom|hhaim|ybrustin|ibarnea) # dev users, 70% python3 30% python2
                     case $(($RANDOM % 10)) in
                         [7-9])
-                            find_python2
+                            find_python2 ${PYTHONS2[*]}
                             ;;
                         *)
-                            find_python3
+                            find_python3 ${PYTHONS3[*]}
                             ;;
                     esac
                     ;;
                 *) # default is find any
-                    (find_python2) &> /dev/null && find_python2 && return
-                    (find_python3) &> /dev/null && find_python3 && return
+                    (find_python2 ${PYTHONS2[*]}) &> /dev/null && find_python2 ${PYTHONS2[*]} && return
+                    (find_python3 ${PYTHONS3[*]}) &> /dev/null && find_python3 ${PYTHONS3[*]} && return
                     echo "Python versions 2.7 or 3.4 or 3.5 required"
                     exit -1
                     ;;
