03c00d34c35389613c76df9c939a5d06c01587af
Yaroslav Brustinov <ybrustin@cisco.com>
2018-05-27 14:09:35 +0300

    Use scapy utils6 for ipv6 address manipulations

--- scripts/automation/trex_control_plane/interactive/trex/stl/trex_stl_packet_builder_scapy.py
+++ scripts/automation/trex_control_plane/interactive/trex/stl/trex_stl_packet_builder_scapy.py
@@ -88,33 +88,19 @@ def increase_ip(ip_str, val = 1):
 def generate_ipv6(mac_str, prefix = 'fe80'):
     mac_arr = mac_str.split(':')
     assert len(mac_arr) == 6, 'mac should be in format of 11:22:33:44:55:66, got: %s' % mac_str
-    mac_arr[0] = '%x' % (int(mac_arr[0], 16) ^ 2) # invert second bit
-    return '%s::%s%s:%sff:fe%s:%s%s' % tuple([prefix] + mac_arr[:3] + mac_arr[3:])
+    return '%s::%s' % (prefix, in6_mactoifaceid(mac_str).lower())
 
 # RFC 4291
 def generate_ipv6_solicited_node(ipv6):
-    ipv6 = expand_ipv6(ipv6)
-    return 'ff02::1:ff%s' % ipv6[-7:]
+    ipv6_buf = is_valid_ipv6_ret(ipv6)
+    solic_buf = in6_getnsma(ipv6_buf)
+    return inet_ntop(socket.AF_INET6, solic_buf)
 
-
-# return full ipv6 ff02::1 -> ff02:0:0:0:0:0:0:1
-def expand_ipv6(addr):
-    addr_arr = addr.split(':')
-    if addr.startswith(':'):
-        addr_arr[0] = '0'
-    if addr.endswith(':'):
-        addr_arr[-1] = '0'
-    for i, e in enumerate(addr_arr):
-        if not e:
-            return ':'.join(addr_arr[:i] + ['0'] * (9 - len(addr_arr)) + addr_arr[i + 1:])
-    return ':'.join(addr_arr)
-
-
+# RFC 1972
 # return multicast mac based on ipv6 ff02::1 -> 33:33:00:00:00:01
-def multicast_mac_from_ipv6(addr):
-    addr = expand_ipv6(addr)
-    addr_arr = addr.split(':')
-    return '33:33:%02x:%02x:%02x:%02x' % (divmod(int(addr_arr[-2], 16), 256) + divmod(int(addr_arr[-1], 16), 256))
+def multicast_mac_from_ipv6(ipv6):
+    ipv6_buf = is_valid_ipv6_ret(ipv6)
+    return in6_getnsmac(ipv6_buf)
 
 
 def is_valid_ipv4_ret(ip_addr):
