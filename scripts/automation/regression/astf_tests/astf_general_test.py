ba3b51ddc5b724aa64a3a7693a546f76c487efcc
Yaroslav Brustinov <ybrustin@cisco.com>
2018-10-27 13:49:54 +0300

    Fix latency not being stopped with end of traffic (not explicit stop)
    Signed-off-by: Yaroslav Brustinov <ybrustin@cisco.com>

--- scripts/automation/regression/astf_tests/astf_general_test.py
+++ scripts/automation/regression/astf_tests/astf_general_test.py
@@ -15,6 +15,8 @@ class CASTFGeneral_Test(CTRexGeneral_Test):
         # check basic requirements, should be verified at test_connectivity, here only skip test
         if CTRexScenario.astf_init_error:
             self.skip(CTRexScenario.astf_init_error)
+        if type(self.astf_trex) is ASTFClient:
+            self.astf_trex.reset()
 
     def connect(self, tries = 10):
         # need delay and check only because TRex process might be still starting
