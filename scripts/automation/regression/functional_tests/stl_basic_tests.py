4a645d9c65a05ebfea0cf5c31f4e724b44f504a9
Besart Dollma <bdollma@cisco.com>
2018-11-20 08:54:53 +0000

    STLVmFlowVarRepeatableRandom split to cores.
    Signed-off-by: Besart Dollma <bdollma@cisco.com>

--- scripts/automation/regression/functional_tests/stl_basic_tests.py
+++ scripts/automation/regression/functional_tests/stl_basic_tests.py
@@ -205,6 +205,7 @@ class CStlBasic_Test(functional_general_test.CGeneralFunctional_Test):
 
     def test_stl_profiles (self):
         p = [
+             ["split_var_to_cores.py", "-m 1 -l 20 -c 2", True],
              ["udp_1pkt_1mac_override.py","-m 1 -l 50",True],
              ["syn_attack.py","-m 1 -l 50",True],
              ["udp_1pkt_1mac.py","-m 1 -l 50",True],
