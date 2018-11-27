1a1166a4ef7a480efd5f7a4f2fdf69d17db5082d
Besart Dollma <bdollma@cisco.com>
2018-11-27 12:11:42 +0000

    Documentation and profile contain value list example with next_var
    Signed-off-by: Besart Dollma <bdollma@cisco.com>

--- scripts/stl/dependent_field_engine_vars.py
+++ scripts/stl/dependent_field_engine_vars.py
@@ -7,12 +7,12 @@ class STLS1(object):
         base_pkt = Ether()/IP()/UDP(dport=12, sport=1025)/ (24 * 'x')
 
         vm = STLVM()
-        vm.var(name='var1', min_value=ord('a'), max_value=ord('d'), size=1, step=1, op='inc', next_var='var2')
+        vm.var(name='IP_src', min_value=None, max_value=None, size=4, op='dec', step=1, value_list = ['16.0.0.1', '10.0.0.1', '14.0.0.1'], next_var='var1')
+        vm.write(fv_name='IP_src', pkt_offset='IP.src')
+        vm.var(name='var1', min_value=ord('a'), max_value=ord('c'), size=1, step=1, op='inc', next_var='var2')
         vm.write(fv_name='var1', pkt_offset=44)
-        vm.var(name='var2', min_value=ord('a'), max_value=ord('c'), size=1, step=1, op='inc', next_var='var3', split_to_cores=False)
+        vm.var(name='var2', min_value=ord('a'), max_value=ord('b'), size=1, step=1, op='inc', split_to_cores=False)
         vm.write(fv_name='var2', pkt_offset=43)
-        vm.var(name='var3', min_value=ord('a'), max_value=ord('b'), size=1, step=1, op='inc', split_to_cores=False)
-        vm.write(fv_name='var3', pkt_offset=42)
 
         return STLStream(packet = STLPktBuilder(pkt = base_pkt, vm = vm),
                          mode = STLTXCont())
