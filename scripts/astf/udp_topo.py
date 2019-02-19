0b6efbea078be61df27cec01ce450dea19bebfe8
Hanoh Haim <hhaim@cisco.com>
2019-02-19 16:06:45 +0200

    astf: add astf topo examples file
    Signed-off-by: Hanoh Haim <hhaim@cisco.com>

---
+++ scripts/astf/udp_topo.py
@@ -0,0 +1,32 @@
+# Example for creating your program by specifying buffers to send, without relaying on pcap file
+
+from trex.astf.topo import *
+
+
+def get_topo():                        
+    topo = ASTFTopology()
+
+# VIFs
+    topo.add_vif(
+        port_id = '0.1',               
+        src_mac = '12:12:12:12:12:12', 
+        src_ipv4 = '5.5.5.5',          
+    )
+
+# GWs
+    topo.add_gw(
+        port_id = '0.1',               
+        src_start = '16.0.0.1',        
+        src_end = '16.0.0.2',          
+        dst = '00:00:00:01:00:02',     
+    )
+
+    topo.add_gw(
+        port_id = '0',                 
+        src_start = '16.0.0.3',
+        src_end = '16.0.0.4',
+        dst = '00:00:00:01:00:02',               
+    )
+
+    return topo
+
---
+++ scripts/astf/udp_topo_traffic.py
@@ -0,0 +1,22 @@
+# Example for creating your program from pcap file
+
+from trex.astf.api import *
+
+class Prof1():
+    def get_profile(self, **kwargs):
+        file = kwargs.get('file', "../avl/delay_dns_0.pcap")
+        # ip generator
+        ip_gen_c = ASTFIPGenDist(ip_range=["16.0.0.1", "16.0.0.4"], distribution="seq")
+        ip_gen_s = ASTFIPGenDist(ip_range=["48.0.0.0", "48.0.255.255"], distribution="seq")
+        ip_gen = ASTFIPGen(glob=ASTFIPGenGlobal(ip_offset="1.0.0.0"),
+                           dist_client=ip_gen_c,
+                           dist_server=ip_gen_s)
+
+        return ASTFProfile(default_ip_gen=ip_gen,
+                            cap_list=[ASTFCapInfo(file=file,
+                            cps=1)])
+
+
+
+def register():
+    return Prof1()
