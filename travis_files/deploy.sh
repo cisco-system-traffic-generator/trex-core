0d9f0a1305f80fc0c3153a071c7e133b0e6a0bb3
elados93 <eaharon@cisco.com>
2019-05-22 23:48:50 +0300

    fix variables, only one comment when done

--- travis_files/deploy.sh
+++ travis_files/deploy.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
-comment_link_to_results() {
-    DOWNLOAD_LINK=$(<Travis_Results/download_link.txt)
-    echo ${DOWNLOAD_LINK}
-    COMMENT="Download your test results [here](${DOWNLOAD_LINK}) (html file)"
-    curl -H "Authorization: token $GITHUB_COMMENT_TOKEN" -X POST -d "{\"body\": \"$COMMENT\"}" "https://api.github.com/repos/cisco-system-traffic-generator/trex-core/issues/$TRAVIS_PULL_REQUEST/comments"
-    rm Travis_Results/download_link.txt
-}
-
-comment_link_to_results
+LINK_DIR=travis_results/download_link.txt
+if [[ -f "$LINK_DIR" ]]; then
+  DOWNLOAD_LINK=$(<$LINK_DIR)
+  echo ${DOWNLOAD_LINK}
+  COMMENT="Download your test results [here](${DOWNLOAD_LINK}) (html file)"
+  curl -H "Authorization: token $GITHUB_COMMENT_TOKEN" -X POST -d "{\"body\": \"$COMMENT\"}" "https://api.github.com/repos/$TRAVIS_REPO_SLUG/issues/$TRAVIS_PULL_REQUEST/comments"
+else
+  echo no results to deploy
+fi
--- travis_files/wait_for_results.py
+++ travis_files/wait_for_results.py
@@ -1,48 +1,39 @@
-import sys
+import argparse
 import os
+import sys
 import time
 
 from google_drive_integration import GoogleDriveService
 
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--last-check", dest="last_check", required=True,
+                        help="True/False if it is the last script checking results")
+    args = parser.parse_args()
 
-def get_latest_sha(pr_number):
-    command = """
-    git ls-remote https://github.com/cisco-system-traffic-generator/trex-core refs/pull/%d/head | awk '{print $1;}'""" % pr_number
-    return os.popen(command).read().strip()
-
-
-def main():
-    if len(sys.argv) < 2:
-        sys.exit('need pr number as arg!')
-    sha = get_latest_sha(int(sys.argv[1]))  # sent by .travis.yml as string
-    is_last = sys.argv[2] == 'True'  # sent by .travis.yml as True / False
-
+    pr_num, sha = os.environ['PR_NUM'], os.environ['SHA']
     gdservice = GoogleDriveService()
     total_sleep, sleeping_between_download = 45, 1  # in min
+    os.mkdir('travis_results')
 
     for _ in range(total_sleep // sleeping_between_download):
         # check if there are new results
         print('-' * 42)
-        print('looking for test results for pull request for sha: %s' % sha)
+        print('looking for test results for pull request number %d for sha: %s' % (pr_num, sha))
         res, passed = gdservice.download_result(sha)
         if res:
             print("found the results!")
-            print("test passes: %s" % passed)
             if not passed:
                 sys.exit("test failed, more information can be found in html file")
             else:
                 print("test passed, look for a comment with html download link at the pull request page")
-            break
+                sys.exit(0)
 
         print("didn't find results, sleeping for %d min..." % sleeping_between_download)
         time.sleep(sleeping_between_download * 60)
-    if is_last:
-        print('-' * 42)
+
+    print('-' * 42)
+    if args.last_check:
         sys.exit('waiting for results timeout! you may restart the build later')
     else:
-        print('-' * 42)
         print('did not find results at this run')
-
-
-if __name__ == '__main__':
-    main()
