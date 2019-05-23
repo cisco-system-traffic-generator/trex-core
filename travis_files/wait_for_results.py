23b7e149cd93ccfd79e2080ed0e1c50c400dbb2d
Elad Aharon <eaharon@cisco.com>
2019-05-23 16:00:54 +0000

    fix flow between jobs in travis

--- travis_files/wait_for_results.py
+++ travis_files/wait_for_results.py
@@ -10,21 +10,26 @@ if __name__ == '__main__':
     pr_num, sha = int(os.environ['PR_NUM']), os.environ['SHA']
 
     gdservice = GoogleDriveService()
-    total_sleep, sleeping_between_download = 45, 1  # in min
     os.mkdir('travis_results')
+    total_sleep, sleeping_between_download = 45, 1  # in min
 
-    for _ in range(total_sleep // sleeping_between_download):
-        # check if there are new results
+    for i in range(total_sleep // sleeping_between_download):
+        # check if there are new results in google drive
         print('-' * 42)
         print('looking for test results for pull request number %d for sha: %s' % (pr_num, sha))
-        res, passed = gdservice.download_result(sha)
-        if res:
-            print("found the results!")
-            if not passed:
-                sys.exit("test failed, more information can be found in html file")
-            else:
-                print("test passed, look for a comment with html download link at the pull request page")
+        file_id, file_name = gdservice.find_file_id(sha)
+
+        if file_id:
+            if i == 0:
+                print('results posted by earlier job, exit with code 0')
                 sys.exit(0)
+            else:
+                passed = gdservice.download_result(file_id, file_name)  # download the results
+                if passed:
+                    print("test passed, look for a comment with html download link at the pull request page")
+                    sys.exit(0)
+                else:
+                    sys.exit("test failed, more information can be found in html file")
 
         print("didn't find results, sleeping for %d min..." % sleeping_between_download)
         time.sleep(sleeping_between_download * 60)
@@ -33,4 +38,4 @@ if __name__ == '__main__':
     if last_check:
         sys.exit('waiting for results timeout! you may restart the build later')
     else:
-        print('did not find results at this run')
+        print('did not find results at this run, another job will continue seeking')
