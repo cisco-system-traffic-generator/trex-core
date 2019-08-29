d1e1168fe9afe84878ec35dbab29484b41922b83
Junsub Han <junsub.han@ericsson.com>
2019-08-29 15:21:05 +0900

    ASTF dynamic profile regression test updated

--- scripts/automation/regression/astf_tests/astf_resilience_test.py
+++ scripts/automation/regression/astf_tests/astf_resilience_test.py
@@ -1,5 +1,7 @@
 import os, sys
 import time
+import random
+import string
 
 from .astf_general_test import CASTFGeneral_Test, CTRexScenario
 from nose.tools import assert_raises
@@ -16,6 +18,10 @@ class ASTFResilience_Test(CASTFGeneral_Test):
             self.skip('not enough memory for this test')
         if setup in ['trex12']:
             self.weak = True
+        self.low_memory = self.weak
+        if setup in ['trex41']:
+            self.low_memory = True # trex-41 uses the memory for the driver and crash
+
 
     def ip_gen(self, client_base, server_base, client_ips, server_ips):
         assert client_ips>0
@@ -54,6 +60,7 @@ class ASTFResilience_Test(CASTFGeneral_Test):
 
         return ASTFProfile(default_ip_gen = ip_gen, templates = templates_arr)
 
+
     def test_astf_params(self):
         print('')
 
@@ -86,6 +93,49 @@ class ASTFResilience_Test(CASTFGeneral_Test):
                     self.astf_trex.stop()
 
 
+    def randomString(self, stringLength=10):
+        """Generate a random string of fixed length """
+        letters = string.ascii_lowercase
+        return ''.join(random.choice(letters) for i in range(stringLength))
+
+
+    def test_astf_params_dynamic_profile(self):
+        print('')
+
+        for client_ips in (1<<8, 1<<16):
+            for server_ips in (1<<8, 1<<16):
+                for templates in (1, 1<<8, 1<<12):
+                    if self.weak:
+                        if ( (templates > 1<<8) or 
+                             (server_ips > 1<<8 ) or 
+                             (client_ips > 1<<8 ) ):
+                           continue
+
+                    params = {
+                        'client_ips': client_ips,
+                        'server_ips': server_ips,
+                        'templates': templates,
+                    }
+
+                    print('Creating profile with params: %s' % params)
+                    profile = self.profile_gen(**params)
+                    profile_str = profile.to_json_str()
+                    print('Profile size: %s' % len(profile_str))
+
+                    start_time = time.time()
+                    print('Creating random name for the dynamic profile')
+                    random_profile = self.randomString()
+                    print('Dynamic profile name : %s' % str(random_profile))
+                    self.astf_trex.load_profile(profile, pid_input=str(random_profile))
+                    print('Load took: %g' % round(time.time() - start_time, 3))
+                    start_time = time.time()
+                    self.astf_trex.start(duration = 1, nc = True, pid_input=str(random_profile))
+                    print('Start took: %g' % round(time.time() - start_time, 3))
+                    self.astf_trex.stop(pid_input=str(random_profile))
+
+        self.astf_trex.reset()
+
+
     def test_double_start_stop(self):
         print('')
         c = self.astf_trex
@@ -97,6 +147,24 @@ class ASTFResilience_Test(CASTFGeneral_Test):
         c.stop()
 
 
+    def test_double_start_stop_dynamic_profile(self):
+        print('')
+        c = self.astf_trex
+        random_profile_1 = self.randomString()
+        print('Dynamic profile(1) : %s' % str(random_profile_1))
+        c.load_profile(os.path.join(CTRexScenario.scripts_path, 'astf', 'udp1.py'), pid_input=str(random_profile_1))
+        c.start(duration = 20, pid_input=str(random_profile_1))
+
+        with assert_raises(TRexError):
+            random_profile_2 = self.randomString()
+            print('Dynamic profile(2) : %s' % str(random_profile_2))
+            c.load_profile(os.path.join(CTRexScenario.scripts_path, 'astf', 'udp1.py'), pid_input=str(random_profile_2))
+            c.start(pid_input=str(random_profile_2))
+
+        c.stop(pid_input=str(random_profile_1))
+        c.stop(pid_input=str(random_profile_2))
+
+
     def test_stress_start_stop(self):
         print('')
         c = self.astf_trex
@@ -105,4 +173,26 @@ class ASTFResilience_Test(CASTFGeneral_Test):
             c.start()
             c.stop()
 
+    def test_stress_start_stop_dynamic_profile(self):
+        print('')
+        c = self.astf_trex
+
+        profiles =1000
+        if self.low_memory:
+            profiles = 100
+ 
+        for n in range(profiles):
+            profile_n = self.randomString()
+            port_n = 9000 + n
+            tunables = {'port': port_n}
+            print('Dynamic profile added : %s' % str(profile_n))
+            print('Port added : %s' % str(port_n))
+            c.load_profile(os.path.join(CTRexScenario.scripts_path, 'astf', 'http_simple_port_tunable.py'),
+                           tunables=tunables,
+                           pid_input=str(profile_n))
+            c.start(duration = 20, pid_input=str(profile_n))
+
+        c.wait_on_traffic()
+        c.stop(pid_input = "*")
+        c.reset()
 
--- scripts/automation/trex_control_plane/interactive/trex/astf/trex_astf_client.py
+++ scripts/automation/trex_control_plane/interactive/trex/astf/trex_astf_client.py
@@ -230,7 +230,7 @@ class ASTFClient(TRexClient):
             else:
                 if profile_id in list(self.astf_profile_state.keys()):
                     if start == True:
-                        if self.is_dynamic and self.astf_profile_state[profile_id] not in ok_states:
+                        if self.is_dynamic and self.astf_profile_state.get(profile_id) not in ok_states:
                             raise TRexError("%s state:Transmitting, should be one of following:Idle, Loaded profile" % profile_id)
                 else:
                     if start == True:
@@ -271,7 +271,7 @@ class ASTFClient(TRexClient):
 
         while True:
             self.sync()
-            state = self.astf_profile_state[pid_input] if self.is_dynamic else self.state
+            state = self.astf_profile_state.get(pid_input) if self.is_dynamic else self.state
             if state not in self.transient_states:
                 break
             time.sleep(0.1)
@@ -282,7 +282,7 @@ class ASTFClient(TRexClient):
             raise TRexError(rc.err())
         self.sync()
 
-    def _transmit_async(self, rpc_func, ok_states, bad_states = None, **k):
+    def _transmit_async(self, rpc_func, ok_states, bad_states = None, is_profile_clear = False, **k):
         profile_id = k['params']['profile_id'] 
         ok_states = listify(ok_states)
         if bad_states is not None:
@@ -300,7 +300,12 @@ class ASTFClient(TRexClient):
                 return rc
             while True:
                 self.sync()
-                state = self.astf_profile_state[profile_id] if self.is_dynamic else self.state
+                state = self.astf_profile_state.get(profile_id) if self.is_dynamic else self.state
+                if is_profile_clear:
+                    if profile_id is not DEFAULT_PROFILE_ID:
+                        if state is None:
+                            return RC_OK()
+
                 if state in ok_states:
                     return RC_OK()
 
@@ -344,7 +349,7 @@ class ASTFClient(TRexClient):
                 self.stop(pid_input = ALL_PROFILE_ID)
                 self.stop_latency()
                 self.clear_stats(ports, pid_input = ALL_PROFILE_ID)
-                self.clear_profile(pid_input = ALL_PROFILE_ID)
+                self.clear_profile(block = True, pid_input = ALL_PROFILE_ID)
                 self.set_port_attr(ports,
                                    promiscuous = False if self.any_port.is_prom_supported() else None,
                                    link_up = True if restart else None)
@@ -524,7 +529,7 @@ class ASTFClient(TRexClient):
 
 
     @client_api('command', True)
-    def clear_profile(self, pid_input = DEFAULT_PROFILE_ID):
+    def clear_profile(self, block = False, pid_input = DEFAULT_PROFILE_ID):
         """
             Clear loaded profile
 
@@ -539,14 +544,16 @@ class ASTFClient(TRexClient):
         valid_pids = self.validate_profile_id_input(pid_input)
 
         for profile_id in valid_pids:
-            if self.astf_profile_state[profile_id] in ok_states:
+            if self.astf_profile_state.get(profile_id) in ok_states:
                 params = {
                     'handler': self.handler,
                     'profile_id': profile_id
                 }
                 self.ctx.logger.pre_cmd('Clearing loaded profile.')
-
-                rc = self._transmit('profile_clear', params = params)
+                if block:
+                    rc = self._transmit_async('profile_clear', params = params, ok_states = self.STATE_IDLE, is_profile_clear = True)
+                else:
+                    rc = self._transmit('profile_clear', params = params)
                 self.ctx.logger.post_cmd(rc)
                 if not rc:
                     raise TRexError(rc.err())
@@ -1454,7 +1461,7 @@ class ASTFClient(TRexClient):
 
         profile_list = list(self.astf_profile_state.keys())
         for profile_id in profile_list:
-            profile_state = self.astf_profile_state[profile_id]
+            profile_state = self.astf_profile_state.get(profile_id)
             state = "Unknown" if profile_state is None else astf_states[profile_state]
             table.add_row([
                 profile_id,
