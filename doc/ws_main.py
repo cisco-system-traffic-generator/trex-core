aba3b61e7ea5d1b4a71972e4c2e2d305a652ec92
Hanoh Haim <hhaim@cisco.com>
2019-10-06 11:44:24 +0300

    v2.64
    Signed-off-by: Hanoh Haim <hhaim@cisco.com>

--- doc/ws_main.py
+++ doc/ws_main.py
@@ -760,6 +760,7 @@ def build(bld):
         'active_flows',
         'fixing_pcaps',
         'software_mode',
+        'azure'
         ]
     for appendix_name in toc_appendixes:
         src_name = 'trex_appendix_%s.asciidoc waf.css' % appendix_name
