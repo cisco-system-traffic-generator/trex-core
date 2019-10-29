eb6b2cea50cd3a2693f86230d17d3ee14bd1d0ef
Hanoh Haim <hhaim@cisco.com>
2019-10-29 14:19:00 +0200

    re enable the tests
    Signed-off-by: Hanoh Haim <hhaim@cisco.com>

--- scripts/automation/regression/astf_tests/astf_resilience_test.py
+++ scripts/automation/regression/astf_tests/astf_resilience_test.py
@@ -136,7 +136,7 @@ class ASTFResilience_Test(CASTFGeneral_Test):
         self.astf_trex.reset()
 
 
-    def _test_double_start_stop(self):
+    def test_double_start_stop(self):
         print('')
         c = self.astf_trex
         c.load_profile(os.path.join(CTRexScenario.scripts_path, 'astf', 'udp1.py'))
@@ -147,7 +147,7 @@ class ASTFResilience_Test(CASTFGeneral_Test):
         c.stop()
 
 
-    def _test_double_start_stop_dynamic_profile(self):
+    def test_double_start_stop_dynamic_profile(self):
         print('')
         c = self.astf_trex
         random_profile_1 = self.randomString()
