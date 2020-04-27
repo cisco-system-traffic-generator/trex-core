43b18a4579eb521d32a038b1353d5c2de98f33c1
Hanoh Haim <hhaim@cisco.com>
2020-04-27 13:10:21 +0300

    Revert "Revert "Removed DP's ASTF DB map access""
    This reverts commit 899984e48d048d7cdd9fa532fd81a27eaff58437.

--- src/stx/astf/trex_astf.cpp
+++ src/stx/astf/trex_astf.cpp
@@ -395,7 +395,9 @@ void TrexAstf::profile_clear(cp_profile_id_t profile_id){
     if (pid->get_profile_state() == STATE_LOADED) {
         pid->profile_change_state(STATE_DELETE);
 
-        TrexCpToDpMsgBase *msg = new TrexAstfDeleteDB(pid->get_dp_profile_id());
+        auto dp_profile_id = pid->get_dp_profile_id();
+        auto astf_db = CAstfDB::instance(dp_profile_id);
+        TrexCpToDpMsgBase *msg = new TrexAstfDeleteDB(dp_profile_id, astf_db);
         send_message_to_dp(0, msg);
     }
     else { // STATE_IDLE
@@ -909,14 +911,16 @@ void TrexAstfPerProfile::parse() {
 
     profile_change_state(STATE_PARSE);
 
-    TrexCpToDpMsgBase *msg = new TrexAstfLoadDB(m_dp_profile_id, prof, topo);
+    auto astf_db = CAstfDB::get_instance(m_dp_profile_id);
+    TrexCpToDpMsgBase *msg = new TrexAstfLoadDB(m_dp_profile_id, prof, topo, astf_db);
     m_astf_obj->send_message_to_dp(0, msg);
 }
 
 void TrexAstfPerProfile::build() {
     profile_change_state(STATE_BUILD);
 
-    TrexCpToDpMsgBase *msg = new TrexAstfDpCreateTcp(m_dp_profile_id, m_factor);
+    auto astf_db = CAstfDB::instance(m_dp_profile_id);
+    TrexCpToDpMsgBase *msg = new TrexAstfDpCreateTcp(m_dp_profile_id, m_factor, astf_db);
     m_astf_obj->send_message_to_all_dp(msg);
 }
 
@@ -950,7 +954,8 @@ void TrexAstfPerProfile::cleanup() {
 
     m_astf_obj->handle_stop_latency();
 
-    TrexCpToDpMsgBase *msg = new TrexAstfDpDeleteTcp(m_dp_profile_id, false);
+    auto astf_db = CAstfDB::instance(m_dp_profile_id);
+    TrexCpToDpMsgBase *msg = new TrexAstfDpDeleteTcp(m_dp_profile_id, false, astf_db);
     m_astf_obj->send_message_to_all_dp(msg);
 }
 
@@ -979,6 +984,7 @@ void TrexAstfPerProfile::all_dp_cores_finished() {
             profile_change_state(STATE_LOADED);
             break;
         case STATE_DELETE:
+            CAstfDB::free_instance(m_dp_profile_id);
             publish_astf_profile_clear();
             {
                 auto astf = m_astf_obj;
