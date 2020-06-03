d403d9790dd623d5302ed24ef35b75e3c2100eb6
Hanoh Haim <hhaim@cisco.com>
2020-06-03 09:48:28 +0300

    mlx5: azure fix for dpdk 20_02 the link is not stable
    Signed-off-by: Hanoh Haim <hhaim@cisco.com>

--- src/dpdk/drivers/net/mlx5/mlx5_ethdev.c
+++ src/dpdk/drivers/net/mlx5/mlx5_ethdev.c
@@ -994,8 +994,16 @@ mlx5_link_update_unlocked_gs(struct rte_eth_dev *dev,
 			dev->data->port_id, strerror(rte_errno));
 		return ret;
 	}
+	#ifdef TREX_PATCH
+    // in case of Azure + failsafe it should have a speed
+	dev_link.link_speed = (ecmd->speed == UINT32_MAX) ? ETH_SPEED_NUM_10G :
+							    ecmd->speed;
+	#else
+
 	dev_link.link_speed = (ecmd->speed == UINT32_MAX) ? ETH_SPEED_NUM_NONE :
 							    ecmd->speed;
+    #endif 
+
 	sc = ecmd->link_mode_masks[0] |
 		((uint64_t)ecmd->link_mode_masks[1] << 32);
 	priv->link_speed_capa = 0;
