e729a10a8613557c611b1cb78d9daf9074529177
Hanoch Haim <hhaim@cisco.com>
2020-09-17 15:57:01 +0300

    add transport plugin
    Signed-off-by: Hanoch Haim <hhaim@cisco.com>

---
+++ scripts/automation/trex_control_plane/interactive/trex/emu/emu_plugins/emu_plugin_transport.py
@@ -0,0 +1,42 @@
+from trex.emu.api import *
+from trex.emu.emu_plugins.emu_plugin_base import *
+import trex.utils.parsing_opts as parsing_opts
+import json
+
+
+class TRANSPORTPlugin(EMUPluginBase):
+    '''Defines Transport plugin  '''
+
+    plugin_name = 'transport'
+
+    # init jsons example for SDK
+    INIT_JSON_NS = {'transport': {}}
+    """
+    :parameters:
+        Empty.
+    """
+
+    INIT_JSON_CLIENT = {'transport': {}}
+    """
+    """
+
+    def __init__(self, emu_client):
+        super(TRANSPORTPlugin, self).__init__(emu_client, 'transport_client_cnt')
+
+    # Plugins methods
+
+    @plugin_api('transport_show_counters', 'emu')
+    def transport_show_counters_line(self, line):
+        '''Show transport counters (per client).\n'''
+        parser = parsing_opts.gen_parser(self,
+                                        "show_counters_transport",
+                                        self.transport_show_counters_line.__doc__,
+                                        parsing_opts.EMU_SHOW_CNT_GROUP,
+                                        parsing_opts.EMU_NS_GROUP,
+                                        parsing_opts.EMU_CLIENT_GROUP,
+                                        parsing_opts.EMU_DUMPS_OPT
+                                        )
+
+        opts = parser.parse_args(line.split())
+        self.emu_c._base_show_counters(self.data_c, opts, req_ns = True)
+        return True
