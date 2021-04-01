76add934dfb56e2af87d543f344f6f6b13d56d94
“rrabi” <rrabi@cisco.com>
2021-04-01 18:00:00 +0300

    update Dhcp Profile To Support MAC & DHCP Options config.

--- scripts/emu/simple_dhcp.py
+++ scripts/emu/simple_dhcp.py
@@ -9,7 +9,7 @@ class Prof1():
         self.def_ns_plugs  = {'igmp' : {'dmac':self.mac.V()}} 
         self.def_c_plugs  = None
 
-    def create_profile(self, ns_size, clients_size):
+    def create_profile(self, ns_size, clients_size,clientMac,discoverDhcpClassIdOption,requestDhcpClassIdOption):
         ns_list = []
 
         # create different namespace each time
@@ -20,7 +20,7 @@ class Prof1():
                                     tpid    = tpid)
             ns = EMUNamespaceObj(ns_key = ns_key, def_c_plugs = self.def_c_plugs)
 
-            mac = self.mac 
+            mac = Mac(clientMac)
             ipv4 = Ipv4('0.0.0.0')
             dg = Ipv4('0.0.0.0')
 
@@ -32,8 +32,11 @@ class Prof1():
                                       plugs   = {'arp': {},
                                                  'icmp': {},
                                                  'igmp': {}, 
-                                                 'dhcp': {}
-                                                },
+                                                 'dhcp': {
+                                                     'discoverDhcpClassIdOption':discoverDhcpClassIdOption,
+                                                     'requestDhcpClassIdOption': requestDhcpClassIdOption,
+                                                    }
+                                              },
                                       )
                 ns.add_clients(client)
             ns_list.append(ns)
@@ -41,8 +44,23 @@ class Prof1():
         return EMUProfile(ns = ns_list, def_ns_plugs = self.def_ns_plugs)
 
     def get_profile(self, tuneables):
-        args = get_args.get_args(tuneables)
-        return self.create_profile(args.ns, args.clients)
+      # Argparse for tunables
+        parser = argparse.ArgumentParser(description='Argparser for simple emu profile.', formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+        parser.add_argument('--ns', type = int, default = 1,
+                    help='Number of namespaces to create')
+        parser.add_argument('--clients', type = int, default = 1,
+                    help='Number of clients to create in each namespace')
+
+        parser.add_argument('--mac', type = str, default = '00:00:00:70:00:01',
+                    help='Mac address of the first client')
+        parser.add_argument('--discoverDhcpClassIdOption', nargs="*", type = str, default = '', 
+                   help='discover Dhcp ClassId Option')
+        parser.add_argument('--requestDhcpClassIdOption', nargs="*", type = str, default = '', 
+                   help='request Dhcp ClassId Option')
+
+
+        args = parser.parse_args(tuneables)
+        return self.create_profile(args.ns, args.clients,args.mac,' '.join(args.discoverDhcpClassIdOption),' '.join(args.requestDhcpClassIdOption))
 
 
 def register():
