f3bbc63c9f63e34ce2e7d561085e53e3f760c6f1
Jeongseok Moon <jeongseok.moon@ericsson.com>
2021-05-04 12:48:20 +0900

    Add bonding dpdk driver

--- src/dpdk_trex_port_attr.cpp
+++ src/dpdk_trex_port_attr.cpp
@@ -20,7 +20,8 @@ limitations under the License.
 using namespace std;
 
 DpdkTRexPortAttr::DpdkTRexPortAttr(uint8_t tvpid, uint8_t repid, bool is_virtual,
-        bool fc_change_allowed, bool is_prom_allowed, bool is_vxlan_fs_allowed, bool has_pci) {
+        bool fc_change_allowed, bool is_prom_allowed, bool is_vxlan_fs_allowed, bool has_pci,
+        bool device_flush_needed) {
 
     m_tvpid = tvpid;
     m_repid = repid;
@@ -36,6 +37,7 @@ DpdkTRexPortAttr::DpdkTRexPortAttr(uint8_t tvpid, uint8_t repid, bool is_virtual
     flag_is_link_change_supported = (set_link_up(true) != -ENOTSUP);
     flag_is_prom_change_supported = is_prom_allowed;
     flag_is_vxlan_fs_supported = is_vxlan_fs_allowed;
+    flag_is_device_flush_needed = device_flush_needed;
     update_device_info();
     update_description();
 }
